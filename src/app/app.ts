5bde0881ca0a5863326f05c14336e424895020f1
CrissyDev <christalriziki@gmail.com>
2026-04-14 16:34:49 +0300

    addong styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar'; 
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    Navbar, 
+    Home
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('TUM-Hackathon');
+  title = 'Zuri Flow';
 }
